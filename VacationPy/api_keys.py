bcfb0366e5539b3253336d5c7e5390fa41eb8168
Rachel Chan <chan.rce@gmail.com>
2020-10-19 01:17:12 -0400

    attempt to fix gitignore error

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "48e463d4d49e3d946d60c3e9ec0e2f06"
+weather_api_key = "YOUR KEY HERE!"
 
 # Google API Key
 g_key = "YOUR KEY HERE!"
--- VacationPy/api_keys_2.py
+++
@@ -1,5 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "48e463d4d49e3d946d60c3e9ec0e2f06"
-
-# Google API Key
-g_key = "AIzaSyDcedm49T3wcS-iu5uBvxuODMsMIBlHWNg"
